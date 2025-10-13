c7e4da0fc9daadeaa31de79aa0ed7e312bdae55e
Azhar Ali <azharalimirjat110@gmail.com>
2025-10-13 23:01:10 +0500

    Made some changes

--- RestAPI/index.js
+++ RestAPI/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
 const users = require('./MOCK_DATA.json')
+const fs = require('fs');
 
 const app = express();
-
 const PORT = 8000;
+
+app.use(express.urlencoded({ extended: false}));
 
 // ROUTES
 app.get('/users', (req, res) => {
@@ -32,7 +33,11 @@ app.get('/api/users/:id', (req, res) => {
 // POST
 app.post('/api/users', (req, res) => {
     // TODO: Create the new user
-    return res.json({ status: "Panding" });
+    const body = req.body;
+    users.push({...body, id: users.length+1});
+    fs.writeFile('./DATA.json', JSON.stringify(users), (err, data) => {
+        return res.json({ status : "Pending"});
+    })
 });
 
 
@@ -50,21 +55,21 @@ app.delete('/api/users/:id', (req, res) => {
 
 
 // Merging with requests which are working on id:
-app.route('/api/users/:id'
-    .get((req, res) => {
-        const id = Number(req.params.id);
-        const user = users.find((user) => user.id === id);
-        return res.json(user);
-    }))
-    .put((req, res) => {
-        // TODO: Edit the user with id=x
-        return res.json({ status: "Panding" })
+// app.route('/api/users/:id'
+//     .get((req, res) => {
+//         const id = Number(req.params.id);
+//         const user = users.find((user) => user.id === id);
+//         return res.json(user);
+//     }))
+//     .put((req, res) => {
+//         // TODO: Edit the user with id=x
+//         return res.json({ status: "Panding" })
 
-    })
-    .delete((req, res) => {
-        // TODO: Delete the user with id=x
-        return res.json({ status: "Panding" });
-    });
+//     })
+//     .delete((req, res) => {
+//         // TODO: Delete the user with id=x
+//         return res.json({ status: "Panding" });
+//     });
 
 app.listen(PORT, () => {
     console.log("Server Started!");
