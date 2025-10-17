53b15ef0ef53c808022374d8c3bf953371347fa5
Azhar Ali <azharalimirjat110@gmail.com>
2025-10-17 23:59:46 +0500

    Rectified Some Mistakes

--- Project-01/controllers/user.js
+++ Project-01/controllers/user.js
@@ -11,18 +11,18 @@ async function handleGetAllUsers(req, res) {
 
 async function handleGetUserByID(req, res) {
     const user = await User.findById(req.params.id);
-    if (!user) return res.status(404).json({ error: 'User not found.' })
+    if (!user) return res.status(404).json({ error: 'User not found!' })
     return res.json(user);
 }
 
 async function handleUpdateUserByID(req, res) {
     await User.findByIdAndUpdate(req.params.id, {job_title: "Flutter Developer"});
-    return res.json({ status: "Success" });
+    return res.json({ status: "Successfully updated!" });
 }
 
 async function handleDeleteUserByID(req, res) {
     await User.findByIdAndDelete(req.params.id);
-    return res.json({ status: "Success" });
+    return res.json({ status: "Successfully deleted" });
 }
 
 async function handleCreateNewUser(req, res) {
@@ -39,7 +39,7 @@ async function handleCreateNewUser(req, res) {
         job_title: body.job_title,
     });
 
-    return res.status(201).json({ msg: "Successfully created!", id: result._id});
+    return res.status(201).json({ msg: "Successfully Added!", id: result._id});
 }
 
 module.exports = {
